432957ccbeaa05f8ecc9b4794c73b6b2c63d6326
yakovenkoroman1993 <feurjarx@gmail.com>
2018-01-10 13:43:29 +0700

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -2,6 +2,7 @@
 
 VERSION="latest"
 COMMAND="node -v"
+PORTS="3001:3001"
 
 # args parser
 POSITIONAL=()
@@ -16,8 +17,13 @@ do
         ;;
         -c|--command)
             COMMAND=${2:-${COMMAND}}
-            shift # past argument
-            shift # past value
+            shift
+            shift
+        ;;
+        -p|--ports)
+            PORTS=${2:-${PORTS}}
+            shift
+            shift
         ;;
         *)    # unknown option
             POSITIONAL+=("$1") # save it in an array for later
@@ -32,6 +38,7 @@ name="node-$(date +%N | sha256sum | base64 | head -c 12)"
 
 docker run --rm \
     --name "${name}" \
+    -p ${PORTS} \
     --mount type=bind,source=$(pwd),target=//app \
     -w //app node:${VERSION} \
     bash -c "${COMMAND}"
