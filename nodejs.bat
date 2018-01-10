2e1f22892c9b8fb312882dcbb0e5113d8950317a
yakovenkoroman1993 <feurjarx@gmail.com>
2018-01-10 13:43:09 +0700

    Update nodejs.bat

--- nodejs.bat
+++ nodejs.bat
@@ -1,23 +1,34 @@
 @ECHO off
 SET VERSION="latest"
 SET COMMAND="node -v"
+SET PORTS="3001:3001"
 
 :loop
 IF NOT "%~1"=="" (
-  IF "%~1"=="-c" (
-  	IF NOT "%~2"=="" ( 
-  		SET COMMAND=%2
-  	)
-    SHIFT
+  IF NOT "%~2"=="" ( 
+    IF "%~1"=="-c" (
+      SET COMMAND=%2
+      SHIFT
+    )
+    IF "%~1"=="-v" (
+      SET VERSION=%2
+      SHIFT
+    ) 
+    IF "%~1"=="-p" (
+      SET PORTS=%2
+      SHIFT
+    ) 
   )
-	IF "%~1"=="-v" (
-		IF NOT "%~2" == "" ( 
-    		SET VERSION=%2
-    	)
-  		SHIFT
-	)	
 	SHIFT
   GOTO :loop
 )
 
-sh nodejs.sh -c %COMMAND% -v %VERSION%
+echo -c %COMMAND% -v %VERSION% -p %PORTS%
+rem sh nodejs.sh -c %COMMAND% -v %VERSION% -p %PORTS%
+
+docker run --rm ^
+    --name nodejs-helper ^
+    -p %PORTS% ^
+    --mount type=bind,source=%cd%,target=//app ^
+    -w //app node:%VERSION% ^
+    bash -c %COMMAND%
